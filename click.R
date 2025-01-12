9d4f77992c9a82295653a18c2982a4fad5b10006
robin <hankin.robin@gmail.com>
2025-01-12 20:42:05 +0000

    whitespace

--- click.R
+++ click.R
@@ -1,4 +1,4 @@
-library(shiny)
+library("shiny")
 
 ui <- fluidPage(
   plotOutput("plot", hover = "plot_hover"),
@@ -9,7 +9,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   output$plot <- renderPlot({
     par(pty = 's')
-    plot(c(-1, 1), c(-1, 1), pch = NA, asp = 1,xlab='a',ylab='b',main='control')
+    plot(c(-1, 1), c(-1, 1), pch = NA, asp = 1, xlab='a', ylab='b', main='control')
     x <- seq(from=0, to=1, by=0.01)
     points(-6*x^2, -8*x^3, type="l")
     points(-6*x^2, +8*x^3, type="l")
@@ -23,7 +23,7 @@ server <- function(input, output) {
   })
 
   output$distPlot <- renderPlot({
-    req(input$plot_hover)  # Ensure plot_click is available
+    req(input$plot_hover)  # Ensure plot_hover is available
     a <- round(input$plot_hover$x, 2)
     b <- round(input$plot_hover$y, 2)
     x <- seq(from = -1, to = 1, by = 0.01)
@@ -31,13 +31,13 @@ server <- function(input, output) {
     x <- seq(from = -1, to=1, by=0.01)
     plot(x,f(x), type="l")
     
-    jj <- polyroot(c(b,2*a,0,4))
+    jj <- polyroot(c(b, 2*a, 0, 4))
     rr <- Re(jj[abs(Im(jj)) < 1e-9])
-    pp <- function(x, ...){points(x,f(x),pch=16,cex=4,...)}
+    pp <- function(x, ...){points(x, f(x), pch=16, cex=4, ...)}
     
     if(length(rr) == 1){
         pp(rr[1],col="red")
-    } else if (length(rr) == 2) {
+    } else if (length(rr) == 2){
         pp(rr[1], col="red")
         pp(rr[2], col="red")
     } else if (length(rr) == 3){
@@ -51,6 +51,5 @@ server <- function(input, output) {
     }
   })
 }
-
 
 shinyApp(ui, server)
